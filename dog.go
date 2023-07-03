288f06ac351e3d7fd3f8e1ce5f6ad57150ad680e
flunkey-cloud <112646056+flunkey-cloud@users.noreply.github.com>
2023-07-03 15:13:21 +0200

    Update dog.go

--- dog.go
+++ dog.go
@@ -1,9 +1,5 @@
 package dog
 
-func Bark() string {
-	return "Bell!"
-}
-
-func Barks() string {
-	return "Bell! Bell! Bell!"
+func WhenGrownUp(s string) string {
+	return "When the dog grows up it does " strings.ToUpper(s)
 }
