af85d27875161e31ae0f582051a3f4159b26f33a
Martin Lobe <flunkey@flunkey.cloud>
2023-07-03 15:17:46 +0200

    fix

--- dog.go
+++ dog.go
@@ -1,5 +1,7 @@
 package dog
 
+import "strings"
+
 func WhenGrownUp(s string) string {
-	return "When the dog grows up it does " strings.ToUpper(s)
+	return "When the dog grows up it does " + strings.ToUpper(s)
 }
